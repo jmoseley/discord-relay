20fe7848813e1c8036a10ef2406fd16e0b64a9aa
Jeremy Moseley <jeremy@convoy.com>
2017-11-01 20:15:44 -0700

    Start express and serve a status.

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,25 @@
+import * as express from 'express';
+
+import StatusHandler from './handlers/status_handler';
 import createLogger from './lib/logger';
 
+const PORT = process.env.PORT || 4001;
 const LOG = createLogger('start');
 
-LOG.info('Starting discord-relay');
+async function start(): Promise<void> {
+  const statusHandler = new StatusHandler();
+
+  LOG.info('Starting discord-relay');
+  const app: express.Application = express();
+  app.get('/status', statusHandler.status);
+
+  // TODO: Logging middleware.
+
+  app.listen(PORT, () => {
+    LOG.info(`Server started on port ${PORT}`);
+  });
+}
+
+start().catch((error: any) => {
+  LOG.error(error);
+});
