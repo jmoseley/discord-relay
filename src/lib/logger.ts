24cd6773332e3a8897fc24071ec982d27883b08f
Jeremy Moseley <jeremy@convoy.com>
2017-11-01 19:54:59 -0700

    Good logging.

---
+++ src/lib/logger.ts
@@ -0,0 +1,20 @@
+import * as winston from 'winston';
+
+function createTransports(loggerName?:string):winston.TransportInstance[] {
+  const transports = [
+    new winston.transports.Console({
+      label: loggerName,
+     }),
+  ];
+  // TODO: Sentry
+  return transports;
+}
+
+export default function createLogger(name:string):winston.LoggerInstance {
+  if (winston.loggers.has(name)) {
+    return winston.loggers.get(name);
+  }
+  return winston.loggers.add(name, {
+    transports: createTransports(name),
+  });
+}
