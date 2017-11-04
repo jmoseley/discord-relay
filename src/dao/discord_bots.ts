507b54ef3ff1122a5599fbf751c1908883dc1621
Jeremy Moseley <jeremy@jeremymoseley.net>
2017-11-03 18:05:32 -0700

    Persist tokens that have been added.

--- src/dao/discord_bots.ts
+++ src/dao/discord_bots.ts
@@ -1,11 +1,28 @@
 import * as AWS from 'aws-sdk';
+import * as _ from 'lodash';
 
-const TABLE_NME = 'DiscordRelay.Bots';
+import createLogger from '../lib/logger';
+
+const LOG = createLogger('DiscordBotsDAO');
+
+const TABLE_NAME = 'DiscordRelay.Bots';
 
 export class DiscordBotsDAO {
   constructor(private readonly dynamoDB: AWS.DynamoDB) {}
 
   public async getAllBotTokens(): Promise<string[]> {
     return [];
+  }
+
+  public async addToken(token: string): Promise<void> {
+    LOG.info(`Saving token ${_.truncate(token, { length: 5 })}`);
+    await this.dynamoDB.putItem({
+      Item: {
+       token: {
+         S: token,
+       },
+      },
+      TableName: TABLE_NAME,
+    }, undefined).promise();
   }
 }
