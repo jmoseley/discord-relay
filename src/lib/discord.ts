a24cc031137b99713759e6fc6423280139e9325f
Jeremy Moseley <jeremy@convoy.com>
2017-11-03 20:38:23 -0700

    Send messages to webhook.

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -1,5 +1,8 @@
 import * as Discord from 'discord.js';
 import * as _ from 'lodash';
+import * as request from 'request-promise-native';
+import * as url from 'url';
+import * as uuid from 'uuid';
 
 import createLogger, { Logger } from './logger';
 
@@ -8,19 +11,48 @@ export default class DiscordMessageHandler {
   private log: Logger;
 
   constructor(private readonly token: string, private readonly webhookUrl: string) {
-    this.log = createLogger(`DiscordMessageHandler(${_.truncate(token, { length: 10 })})`);
+    this.log = createLogger(`DiscordMessageHandler(${token})`);
     this.discordClient = new Discord.Client();
 
+    this.configureClient();
+  }
+
+  public async start() {
+    await this.discordClient.login(this.token);
+  }
+
+  private configureClient(): void {
     this.discordClient.on('ready', () => {
       this.log.info(`Discord Client is ready.`);
     });
 
     this.discordClient.on('message', (message: string) => {
-      this.log.info(`Got a message: '${message}' for token ${_.truncate(this.token, { length: 10 })}`);
+      // Message isn't actually a string yet.
+      message = message.toString();
+      const messageId = uuid.v4();
+
+      this.log.info(`${messageId}: Triggering webhook for message '${message}' to '${this.webhookUrl}'`);
+
+      const updatedUrl = this.appendMessageToUrl(this.webhookUrl, message, messageId);
+
+      // Don't use await since the on handler dosen't support it.
+      request.get({
+        url: updatedUrl,
+      }).then(() => {
+        this.log.info(`${messageId}: Finished calling webhook.`);
+      }).catch((err: any) => {
+        this.log.error(`${messageId}: Error handling webhook:\nWebhook URL: ${updatedUrl}\nError:\n${err}`);
+      });
     });
   }
 
-  public async start() {
-    await this.discordClient.login(this.token);
+  private appendMessageToUrl(urlString: string, message: string, messageId: string): string {
+    const parsedUrl = url.parse(urlString, true);
+    parsedUrl.query.message = message;
+    parsedUrl.query.messageId = messageId;
+    // Re-render the query params.
+    delete parsedUrl.search;
+
+    return url.format(parsedUrl);
   }
 }
