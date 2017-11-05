2dbef98e32f9d8f865c39ad6e37035178e98af3a
Jeremy Moseley <jeremy@jeremymoseley.net>
2017-11-04 18:41:21 -0700

    Get user from cookie.

--- src/handlers/page_handler.ts
+++ src/handlers/page_handler.ts
@@ -2,6 +2,10 @@ import * as express from 'express';
 import * as autobind from 'protobind';
 import * as querystring from 'querystring';
 
+import createLogger from '../lib/logger';
+
+const LOG = createLogger('PageHandler');
+
 export class PageHandler {
   constructor(
     private readonly clientId?: string,
@@ -13,7 +17,8 @@ export class PageHandler {
     }
   }
 
-  public async index(req: express.Request, res: express.Response): Promise<void> {
+  // req is an express.Request
+  public async index(req: any, res: express.Response): Promise<void> {
     // Aligns with Authorization Url from https://discordapp.com/developers/docs/topics/oauth2#authorization-code-grant.
     const oauthParams = {
       client_id: this.clientId,
@@ -25,6 +30,7 @@ export class PageHandler {
 
     res.render('index', {
       oauthParams: querystring.stringify(oauthParams),
+      user: req.user,
      });
   }
 }
