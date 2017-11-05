4b0d1bfbee2ff39f55ade14f40de5a03a773d013
Jeremy Moseley <jeremy@convoy.com>
2017-11-04 21:03:17 -0700

    Store active clients mapped to the token id.

--- src/actions/discord.ts
+++ src/actions/discord.ts
@@ -1,6 +1,6 @@
 import * as _ from 'lodash';
 
-import { DiscordBotsDAO } from '../dao';
+import { DiscordBotsDAO, IToken } from '../dao';
 import DiscordMessageHandler from '../lib/discord';
 import createLogger from '../lib/logger';
 
@@ -12,18 +12,27 @@ export const TOKEN_DOES_NOT_EXIST_ERROR = 'TOKEN_DOES_NOT_EXIST';
 export const TOKEN_DOES_NOT_BELONG_TO_USER_ERROR = 'TOKEN_DOES_NOT_BELONG_TO_USER';
 
 export class DiscordClientActions {
-  private activeClients: DiscordMessageHandler[];
+  private activeClients: {
+    [key: string]: DiscordMessageHandler;
+  };
 
   constructor(private readonly discordTokenDao: DiscordBotsDAO) {
-    this.activeClients = [];
+    this.activeClients = {};
   }
 
   // TODO: Move this into a worker.
   public async startPersistedClients(): Promise<void> {
     const clients = await this.discordTokenDao.getAllTokenHooks();
 
-    this.activeClients = _.compact(await Promise.all(
-      clients.map(client => this.startClient(client.token, client.webhookUrl))));
+    const activeClients = _.compact(await Promise.all(_.map(
+      clients,
+      (client: IToken) => this.startClient(client.token, client.webhookUrl, client.tokenId),
+    )));
+
+    this.activeClients = _(activeClients)
+      .map((client: DiscordMessageHandler) => [client.tokenId, client])
+      .fromPairs()
+      .value();
   }
 
   public async getUserTokens(userId: string) {
@@ -32,11 +41,16 @@ export class DiscordClientActions {
     });
   }
 
-  public async addClient(token: string, webhookUrl: string, userId: string): Promise<void> {
-    await this.discordTokenDao.addToken(token, webhookUrl, userId);
-    const client = await this.startClient(token, webhookUrl);
+  public async addClient(tokenString: string, webhookUrl: string, userId: string): Promise<void> {
+    const token = await this.discordTokenDao.addToken(tokenString, webhookUrl, userId);
+    // If no token, it already exists, which means it is already running.
+    if (!token) {
+      return;
+    }
+
+    const client = await this.startClient(tokenString, webhookUrl, token.tokenId);
     if (client) {
-      this.activeClients.push(client);
+      this.activeClients[token.tokenId] = client;
     }
   }
 
@@ -54,7 +68,7 @@ export class DiscordClientActions {
   }
 
   // TODO: Move this into a worker.
-  private async startClient(token: string, webhookUrl: string): Promise<DiscordMessageHandler | null> {
+  private async startClient(token: string, webhookUrl: string, tokenId: string): Promise<DiscordMessageHandler | null> {
     LOG.info(`Starting client for token ${_.truncate(token, { length: 10 })}`);
     if (!webhookUrl) {
       LOG.info(`Not starting client for token ` +
@@ -62,7 +76,7 @@ export class DiscordClientActions {
       return null;
     }
 
-    const messageHandler = new DiscordMessageHandler(token, webhookUrl);
+    const messageHandler = new DiscordMessageHandler(token, webhookUrl, tokenId);
     await messageHandler.start();
 
     return messageHandler;
--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -10,7 +10,11 @@ export default class DiscordMessageHandler {
   private discordClient: Discord.Client;
   private log: Logger;
 
-  constructor(private readonly token: string, private readonly webhookUrl: string) {
+  constructor(
+    private readonly token: string,
+    private readonly webhookUrl: string,
+    public readonly tokenId: string,
+  ) {
     this.log = createLogger(`DiscordMessageHandler(${token})`);
     this.discordClient = new Discord.Client();
 
